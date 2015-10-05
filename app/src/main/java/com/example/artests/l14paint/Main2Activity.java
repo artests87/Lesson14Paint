da70c32990063771bd74f549804de9003012d836
artests <artests87@gmail.com>
2015-10-05 16:43:43 +0300

    The end

--- app/src/main/java/com/example/artests/l14paint/Main2Activity.java
+++ app/src/main/java/com/example/artests/l14paint/Main2Activity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class Main2Activity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
