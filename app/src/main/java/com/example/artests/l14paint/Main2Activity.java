d523b2fef90f22f0871dc8f439e71420c8bab117
artests <artests87@gmail.com>
2015-10-05 16:39:34 +0300

    The end

--- app/src/main/java/com/example/artests/l14paint/Main2Activity.java
+++ app/src/main/java/com/example/artests/l14paint/Main2Activity.java
@@ -10,5 +10,6 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Draw2D draw2D = new Draw2D(this);
         setContentView(draw2D);
+        //
     }
 }
